ae1d7de7e1d77a3bae3a3d18417a86e5b60341e5
Rono <victorrono18@gmail.com>
2023-10-17 13:33:24 +0300

    hero-section

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-let navbar = document.querySelector('.header .navbar');
-
+let navbar = document.querySelector(".header .navbar");
+let contactInfo = document.querySelector(".contact-info");
 //toggling the navbar on the burger
-document.querySelector('menu-btn').onClick = () => {
-    navbar.classList.toggle('active')
-}
+document.querySelector("menu-btn").onClick = () => {
+  navbar.classList.toggle("active");
+};
+
+//open/close the contacts -bar (info-btn)
+document.querySelector("#info-btn").onClick = () => {
+  contactInfo.classList.add("active");
+};
+
+document.querySelector("#close-contact-info").onClick = () => {
+  contactInfo.classList.remove("active");
+};
 
 window.onscroll = () => {
-    navbar.classList.remove('active');
-}
-
+  navbar.classList.remove("active");
+  contactInfo.classList.remove("active")
+};
